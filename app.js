8268464e509d3040e5b40d9a42f0bf1618cb86cd
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-28 18:25:20 -0500

    Add Employee done

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const inquirer=require("inquirer");
 const connection=require("./connection.js");
+const db=require("./db.js");
+const managers=[];
+const roles=[];
+const departments=[];
+
 
 function init(){
     
@@ -23,7 +28,10 @@ function mainMenu(){
     }) .then(answers=>{
         switch(answers.menuChoice){
             case "Add Employee":
-            addEmployee();
+                addEmployeeManagers();
+            break;
+            case "Add Role":
+                addRoleDepartments();
             break;
 
             case "Exit":
@@ -31,8 +39,62 @@ function mainMenu(){
         }
     });
 }
-function addEmployee(){
-
+function addEmployeeManagers(){
+    db.loadManagers(addEmployeeRoles);
+}
+function addEmployeeRoles(managers){
+    db.loadRoles(addEmployeePrompt, managers);
 }
 
+
+function addEmployeePrompt(managers, roles){
+    // We get an array of manager objects and an array of role objects
+
+    const mChoice=[];
+    const rChoice=[];
+    for(m of managers){
+        let test={};
+        test.value=m.id;
+        test.name=m.first_name+" "+m.last_name+", Manager of "+m.name,
+        mChoice.push(test);
+    }
+    for(r of roles){
+        let test={};
+        test.value=r.id;
+        test.name=r.title+", "+r.name+" Department",
+        rChoice.push(test);
+    }    
+    if(mChoice.length===0) mChoice.push("No manager assigned");
+    inquirer.prompt([
+        {
+            message:"Employee first name: ",
+            name:"firstName",
+            type:"input",
+            validate:function(value){if(value.length>0)return true; else return "Please enter a string."}
+        },       
+        {
+            message:"Employee Last name: ",
+            name:"lastName",
+            type:"input",
+            validate:function(value){if(value.length>0)return true; else return "Please enter a string.";}
+        },       
+        {
+            message:"Employee role: ",
+            name:"roleChoice",
+            type:"list",
+            choices:rChoice
+        },       
+        {
+            message:"Employee manager: ",
+            name:"managerChoice",
+            type:"list",
+            choices:mChoice
+        }
+ 
+    ]).then(
+        (answers)=>{
+            db.addEmployeeInfo(answers, mainMenu);
+        }
+    )
+}
 init();
--- db.js
+++ db.js
@@ -1,22 +1,46 @@
 const connection=require("./connection.js");
 
-function loadManagers(){
-    connection.query("SELECT * FROM employee where ", function(err, result) {
+
+
+function loadManagers(callback){
+
+    // Get a list of every employee that has a role_id that is a manager role
+    let meSpeakWithAManager="select employee.first_name, employee.last_name, employee.id, department.name from employee join role ON role.id=employee.id and role.title='manager' join department on role.department_id=department.id;";
+    connection.query(meSpeakWithAManager, function(err, result) {
         if(err) console.log(err);
         else{
-            let html="";
-            for(actor of result){
-                html+=`ID: ${actor.id}, Name: ${actor.name}, a ${actor.attitude} person with ${actor.coolness_points} coolness points.`+"\n";
-            }
-            res.send(html);
-            res.end();
+            callback(result);
         }
+    });
+}
+
+function loadRoles(callback, prevArray){
+
+    // Get a list of every employee that has a role_id that is a manager role
+    connection.query("select role.id, role.title, department.name from role join department on role.department_id=department.id;", function(err, result) {
+        if(err) console.log("LoadRoles: "+err);
+        else{
+            if(result.length===0){
+                console.log("Please enter at least one role before creating employees.");
+                // Does this work? I wonder if this works....
+                mainMenu();
+            }
+            else callback(prevArray, result);
+        }
+    });
+}
+function addEmployeeInfo(obj, callback){
+    console.log(obj);
+    connection.query("INSERT into employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);", [obj.firstName, obj.lastName, obj.roleChoice, obj.managerChoice],function(err, res){
+        if(err) console.log("Add employee SQL error: "+err);
+        else callback();
     });
 }
 
 
 
-
 module.exports={
-    getCastData
+    loadManagers,
+    loadRoles,
+    addEmployeeInfo
 }
